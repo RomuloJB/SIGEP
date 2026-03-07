11dc5bf383ea8fcd992edeecc9b2aed3c2a8c554
RomuloJB <20230006183@estudantes.ifpr.edu.br>
2026-03-06 21:12:40 -0300

    add | templates html e css

--- website/urls.py
+++ website/urls.py
@@ -1,8 +1,48 @@
 from django.urls import path
-from .views import IndexView, ContactView
+from .views import (
+    CompanyDetailView, CompanyListView, IndexView, ContactView, AboutView, CompanyView,
+    ManagerView, ManagerListView, ManagerDetailView,
+    VendorView, VendorListView, VendorDetailView,
+    ClientView, ClientListView, ClientDetailView,
+    ProductView, ProductListView, ProductDetailView,
+    PedidoView, PedidoListView, PedidoDetailView
+)
 
 urlpatterns = [
     # path('url/navegador', Class.as_view(), name='nome_do_link'),
     path("", IndexView.as_view(), name="index"),
     path("contact/", ContactView.as_view(), name="contact"),
+    path("sobre/", AboutView.as_view(), name="about"),
+
+    # Empresas
+    path("company/", CompanyView.as_view(), name="company"),
+    path("company/list/", CompanyListView.as_view(), name="company_list"),
+    path("company/detail/", CompanyDetailView.as_view(), name="company_detail"),
+
+    
+    # Gerentes
+    path("manager/", ManagerView.as_view(), name="manager"),
+    path("manager/list/", ManagerListView.as_view(), name="manager_list"),
+    path("manager/detail/", ManagerDetailView.as_view(), name="manager_detail"),
+    
+    # Representantes
+    path("vendor/", VendorView.as_view(), name="vendor"),
+    path("vendor/list/", VendorListView.as_view(), name="vendor_list"),
+    path("vendor/detail/", VendorDetailView.as_view(), name="vendor_detail"),
+    
+    # Clientes
+    path("client/", ClientView.as_view(), name="client"),
+    path("client/list/", ClientListView.as_view(), name="client_list"),
+    path("client/detail/", ClientDetailView.as_view(), name="client_detail"),
+    
+    # Produtos
+    path("product/", ProductView.as_view(), name="product"),
+    path("product/list/", ProductListView.as_view(), name="product_list"),
+    path("product/detail/", ProductDetailView.as_view(), name="product_detail"),
+    
+    # Pedidos
+    path("pedido/", PedidoView.as_view(), name="pedido"),
+    path("pedido/list/", PedidoListView.as_view(), name="pedido_list"),
+    path("pedido/detail/", PedidoDetailView.as_view(), name="pedido_detail"),
+
 ]
--- website/views.py
+++ website/views.py
@@ -6,3 +6,72 @@ class IndexView(TemplateView):
 
 class ContactView(TemplateView):
     template_name = "website/contact.html"
+
+class AboutView(TemplateView):
+    template_name = "website/about.html"
+
+# empresas
+class CompanyView(TemplateView):
+    template_name = "website/company.html"
+
+class CompanyListView(TemplateView):
+    template_name = "website/company_list.html"
+
+class CompanyDetailView(TemplateView):
+    template_name = "website/company_detail.html"
+
+# gerentes
+class ManagerView(TemplateView):
+    template_name = "website/manager.html"
+
+class ManagerListView(TemplateView):
+    template_name = "website/manager_list.html"
+
+class ManagerDetailView(TemplateView):
+    template_name = "website/manager_detail.html"
+
+
+# representantes
+class VendorView(TemplateView):
+    template_name = "website/vendor.html"
+
+class VendorListView(TemplateView):
+    template_name = "website/vendor_list.html"
+
+class VendorDetailView(TemplateView):
+    template_name = "website/vendor_detail.html"
+
+
+# clientes
+class ClientView(TemplateView):
+    template_name = "website/client.html"
+
+class ClientListView(TemplateView):
+    template_name = "website/client_list.html"
+
+class ClientDetailView(TemplateView):
+    template_name = "website/client_detail.html"
+
+
+# produtos
+class ProductView(TemplateView):
+    template_name = "website/product.html"
+
+class ProductListView(TemplateView):
+    template_name = "website/product_list.html"
+
+class ProductDetailView(TemplateView):
+    template_name = "website/product_detail.html"
+
+
+# pedidos
+class PedidoView(TemplateView):
+    template_name = "website/pedido.html"
+
+class PedidoListView(TemplateView):
+    template_name = "website/pedido_list.html"
+
+class PedidoDetailView(TemplateView):
+    template_name = "website/pedido_detail.html"
+
+# 
