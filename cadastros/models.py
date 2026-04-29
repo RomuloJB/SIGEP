f4b46c87fe414de60a47bce58de9fce21bccfbda
RomuloJB <jordaobogoni@gmail.com>
2026-04-29 12:21:39 -0300

    fix | pequenas correçoes no models e começo do formulario

--- cadastros/models.py
+++ cadastros/models.py
@@ -26,25 +26,35 @@ class User_Profile(models.Model):
     def __str__(self):
         return "{}".format(self.name)
 
+class MeasureUnit(models.TextChoices):
+    UNIT = "UND", "Unidade"
+    KIT = "KIT", "Kit"
+    CX6 = "CX6", "Caixa com 6 peças"
+    CX7 = "CX7", "Caixa com 7 peças"
+    CX8 = "CX8", "Caixa com 8 peças"
+
+class PaymentMethod(models.TextChoices):
+    DINHEIRO = "1", "Dinheiro"
+    PIX = "2", "Pix"
+    BOLETO = "3", "Boleto(30)"
+    BOLETO15 = "4", "Boleto(15)"
+    BOLETO7 = "5", "Boleto(7)"
+    CREDITO = "6", "Crédito"
+    DEBITO = "7", "Débito"
+    CREDIARIO =  "8", "Crediário"
+
 class Order(models.Model):
     # type = models.??
     company = models.ForeignKey(Company, on_delete=models.PROTECT)
     # sales_rep = models.??()
     # client = models.??()
     field = models.ForeignKey(Field, on_delete=models.PROTECT)
-    payment_method = models.Payment(verbose_name="Método de Pagamento")
+    payment_method = models.CharField(max_length=20, choices=PaymentMethod.choices, default=PaymentMethod.PIX, verbose_name="Método de Pagamento")
     total_value = models.FloatField(verbose_name="Valor Total")
     address = models.CharField(max_length=255, verbose_name="Endereço")
 
     def __str__(self):
         return "{} | {} -> {} = {}".format(self.id, self.client, self.sales_rep, self.value)
-
-class MeasureUnit(models.TextChoices):
-    UNIT = "UND", "Unidade"
-    KIT = "KIT"
-    CX6 = "CX6", "Caixa com 6 peças"
-    CX7 = "CX7", "Caixa com 7 peças"
-    CX8 = "CX8", "Caixa com 8 peças"
 
 class Product(models.Model):
     field = models.ForeignKey(Field, on_delete=models.PROTECT)
@@ -52,7 +62,7 @@ class Product(models.Model):
     color = models.CharField(max_length=50, verbose_name="Cor")
     unit_value = models.FloatField(verbose_name="Valor Unitario")
     stock = models.PositiveIntegerField(verbose_name="Estoque")
-    measure_unit = models.CharField(choices=MeasureUnit.choices, default=MeasureUnit.UN, verbose_name="Unidade de Medida")
+    measure_unit = models.CharField(max_length=5, choices=MeasureUnit.choices, default=MeasureUnit.UNIT, verbose_name="Unidade de Medida")
     company = models.ForeignKey(Company, on_delete=models.PROTECT)
 
     def __str__(self):
